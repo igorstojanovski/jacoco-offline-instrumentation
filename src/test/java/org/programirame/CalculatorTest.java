af35990171d301b1df8e1bb0e84496a47d42434f
Igor Stojanovski <igorce@gmail.com>
2018-02-28 22:48:11 +0100

    Rename main package

--- src/test/java/org/programirame/CalculatorTest.java
+++ src/test/java/org/programirame/CalculatorTest.java
@@ -1,5 +1,6 @@
 package org.programirame;
 
+import org.igorski.Calculator;
 import org.junit.Before;
 import org.junit.Test;
 
--- src/test/java/org/programirame/ComplexCalculatorIT.java
+++ src/test/java/org/programirame/ComplexCalculatorIT.java
@@ -1,5 +1,6 @@
 package org.programirame;
 
+import org.igorski.ComplexCalculator;
 import org.junit.Before;
 import org.junit.Test;
 
